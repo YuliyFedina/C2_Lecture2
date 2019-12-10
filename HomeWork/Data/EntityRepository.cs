d589485c4a6ce39017c33b718ddd901edd08ac13
Федина Юлия <yu.belova@2gis.ru>
2019-12-10 16:19:19 +0700

    fix comment

--- HomeWork/Data/EntityRepository.cs
+++ HomeWork/Data/EntityRepository.cs
@@ -6,6 +6,7 @@ namespace HomeWork.Data.Data
 {
     public class EntityRepository<TEntity> : IRepository<TEntity> where TEntity : IEntity
     {
+        public delegate TEntity FilterRepository(long id);
         private readonly List<TEntity> _storage = new List<TEntity>();
 
         public void Add(TEntity contact)
--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -6,27 +6,18 @@ namespace HomeWork.Data
 {
     class Program
     {
-        delegate IRepository<TEntity> RepositoryFactory<TEntity>() where TEntity : IEntity;
         static void Main(string[] args)
         {
             var user = new User { Id = 1, Name = "Name" };
             var phone = new Phone { Id = 1, PhoneCode = "123", Value = "123124" };
 
-            RepositoryOperation(GetRepository<User>, r => r.Add(user));
+            var userRepository = GetRepository<User>();
+            userRepository.Add(user);
 
-            RepositoryOperation(GetRepository<Contact>, r =>
-            {
-                r.Add(phone);
-                Console.WriteLine(r.GetById(1));
-            });
+            var contactRepository = GetRepository<Contact>();
+            contactRepository.Add(phone);
 
-            Console.ReadLine();
-        }
-
-        private static void RepositoryOperation<T>(RepositoryFactory<T> factory, Action<IRepository<T>> operation)
-            where T : IEntity
-        {
-            operation(factory());
+            Console.WriteLine(contactRepository.GetById(1));
         }
 
         private static IRepository<TEntity> GetRepository<TEntity>() where TEntity : IEntity
